a30124318d6e1a6bb3d9c303640be8cf9db57d15
Usha Lama <ushalama200@gmail.com>
2024-07-06 21:29:06 +0100

    form to add student is created

---
+++ src/pages/StudentAdd.js
@@ -0,0 +1,154 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Loading from "../components/Loading";
+import * as APIService from "./api";
+function StudentAdd() {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [inputErrorList, setInputErrorList] = useState({});
+  const [student, setStudent] = useState({
+    message: "",
+    name: "",
+    email: "",
+    phone: "",
+    course: "",
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setStudent({ ...student, [e.target.name]: e.target.value });
+  };
+
+  const saveStudent = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const response = await APIService.createStudent(
+      student.name,
+      student.email,
+      student.phone,
+      student.course
+    );
+    if (response.ok) {
+      setStudent({
+        message: "student added successfully",
+        name: "",
+        email: "",
+        phone: "",
+        course: "",
+      });
+    }
+
+    // const data = {
+    //   name: student.name,
+    //   email: student.email,
+    //   phone: student.phone,
+    //   course: student.course,
+    // };
+    // axios
+    //   .post(`http://127.0.0.1:8000/api/students`, data)
+    //   .then((res) => {
+    //     alert(res.data.message);
+    //     navigate("/student");
+    //     setLoading(false);
+    //   })
+    //   .catch(function (error) {
+    //     if (error.response) {
+    //       if (error.response.status === 422) {
+    //         setInputErrorList(error.response.data.errors);
+    //         setLoading(false);
+    //       }
+    //       if (error.response.status === 404) {
+    //         alert(error.response.data.message);
+    //         setLoading(false);
+    //       }
+    //       if (error.response.status === 500) {
+    //         alert(error.response.data);
+    //         setLoading(false);
+    //       }
+    //     }
+    //   });
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-header">
+                <h4>
+                  Add Student
+                  <Link to="/student" className="btn btn-danger float-end">
+                    Back
+                  </Link>
+                </h4>
+              </div>
+              <div className="card-body">
+                <form onSubmit={saveStudent}>
+                  <div className="mb-3">
+                    <label>Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      value={student.name}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                    <span className="text-danger">{inputErrorList.name}</span>
+                  </div>
+                  <div className="mb-3">
+                    <label>Email</label>
+                    <input
+                      type="text"
+                      name="email"
+                      value={student.email}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                    <span className="text-danger">{inputErrorList.email}</span>
+                  </div>
+                  <div className="mb-3">
+                    <label>Phone</label>
+                    <input
+                      type="text"
+                      name="phone"
+                      value={student.phone}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                    <span className="text-danger">{inputErrorList.phone}</span>
+                  </div>
+                  <div className="mb-3">
+                    <label>Course</label>
+                    <input
+                      type="text"
+                      name="course"
+                      value={student.course}
+                      onChange={handleInput}
+                      className="form-control"
+                    />
+                    <span className="text-danger">{inputErrorList.course}</span>
+                  </div>
+                  <div className="mb-3">
+                    <button
+                      type="submit"
+                      value="Post"
+                      className="btn btn-primary"
+                    >
+                      Save Student
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default StudentAdd;
