17de4efb216f6c5af3b9c5725a6c73aec2fce528
Usha Lama <ushalama200@gmail.com>
2024-07-07 09:25:17 +0100

    bootstrap installed

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Student from "../pages/Student";
 import StudentCreate from "../pages/StudentCreate";
 import StudentEdit from "../pages/StudentEdit";
+import Home from "../pages/Home";
 
 function MyRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Student />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/student" element={<Student />} />
       <Route path="/student/create" element={<StudentCreate />} />
       <Route path="/student/:id/edit" element={<StudentEdit />} />
     </Routes>
