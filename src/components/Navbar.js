c7b266bb99f7caa566562ba8bb354056c069ba29
Usha Lama <ushalama200@gmail.com>
2024-07-08 00:05:52 +0100

    home tab removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,17 +20,7 @@ function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/home">
-                Home
-              </Link>
-            </li>
-            {/* <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact Us
-              </Link>
-            </li> */}
-            <li className="nav-item">
+                     <li className="nav-item">
               <Link className="nav-link" to="/student">
                 Student
               </Link>
